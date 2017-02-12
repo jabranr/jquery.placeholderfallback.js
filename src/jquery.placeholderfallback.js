9880c3d04ddede5ee1756b423a3acf77c6930b9e
Jabran Rafique <hello@jabran.me>
2017-02-12 20:33:49 +0000

    improve perf

--- src/jquery.placeholderfallback.js
+++ src/jquery.placeholderfallback.js
@@ -1,30 +1,32 @@
 /**
- * This is a small jQuery plugin ot provide fallback support
- * for legacy browsers such as IE9 and lower
+ * Small jQuery plugin to provide placeholder fallback support
+ * in HTML forms for legacy browsers such as IE9 and lower
  *
  * @author: Jabran Rafique <hello@jabran.me>
  * @license: MIT License
- * 
- *
  */
 
-(function($)	{
-	$.fn.placeholderfallback = function()	{
-		return this.each(function()	{
-			var $this = $(this);
-			$this.css('color', '#999');
-			$this.val($this.attr('placeholder'));
+ !(function($)	{
+ 	$.fn.placeholderfallback = function()	{
+ 		var $body = $('body');
 
-			$this.on('focus', function()	{
-				( $this.val() !== '' && $this.val() == $this.attr('placeholder') ) ? $this.val('') : false;
-				$this.css('color', $('body').css('color') ? $('body').css('color') : '#333' );
-			});
+ 		return this.each(function()	{
+ 			var $this = $(this);
+ 			$this.css('color', '#999');
+ 			$this.val($this.attr('placeholder'));
 
-			$this.on('blur', function()	{
-				( $this.val() == '' ) ? $this.val($this.attr('placeholder')) : false;
-				if ( $this.val() == $this.attr('placeholder') )
-					$this.css('color', '#999');
-			});
-		});
-	};
-})(jQuery);
+ 			$this.on('focus', function()	{
+ 				($this.val() !== '' && $this.val() === $this.attr('placeholder')) ? $this.val('') : false;
+ 				$this.css('color', ($body.css('color') ? $body.css('color') : '#333'));
+ 			});
+
+ 			$this.on('blur', function()	{
+ 				($this.val() === '') ? $this.val($this.attr('placeholder')) : false;
+
+ 				if ($this.val() === $this.attr('placeholder')) {
+ 					$this.css('color', '#999');
+ 				}
+ 			});
+ 		});
+ 	};
+ })(jQuery);
